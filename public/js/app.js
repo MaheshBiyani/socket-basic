222922526720b5574e99973acae7545019c636a5
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-15 13:13:29 +0530

    socket io /add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,22 @@
 var socket = io();
 
-socket.on('connect', function(){
+socket.on('connect', function() {
 	console.log('client connected to server')
 })
 
-socket.on('message', function (message){
+socket.on('message', function(message) {
 	console.log("custom event invoked")
 	console.log(message.text);
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 })
 
 
-var $form= jQuery('#message-form');
-$form.on('submit', function(event){
+var $form = jQuery('#message-form');
+$form.on('submit', function(event) {
 	var $message = $form.find('input[name=message]')
 	event.preventDefault();
 	socket.emit('message', {
-		text : $message.val()
+		text: $message.val()
 	});
 	$message.val('');
 });
