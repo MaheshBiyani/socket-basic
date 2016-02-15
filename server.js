b777c6fee04c498df3c0d40d0408358899f9bdfb
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-15 10:37:03 +0530

    added custom javascrit file

---
+++ server.js
@@ -0,0 +1,16 @@
+var PORT = process.env.PORT || 3000;
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection' , function(){
+	console.log("user connected to socket io");
+});
+
+app.use(express.static(__dirname +"/public"));
+
+http.listen(PORT , function (){
+	console.log("server has been started")
+})
