da32623d15293524ca3c64c113042c09028ebb8f
Mahesh Biyani <unikmahesh@gmail.com>
2016-02-16 11:45:08 +0530

    add current user comand

--- server.js
+++ server.js
@@ -7,50 +7,74 @@ var io = require('socket.io')(http);
 var moment = require('moment');
 
 var clientInfo = {};
-io.on('connection' , function(socket){
+
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+	if (typeof info === 'undefined') {
+		return;
+	}
+	Object.keys(clientInfo).forEach(function(socketId) {
+		var userInfo = clientInfo[socketId];
+		if (userInfo.room === info.room) {
+			users.push(userInfo.name);
+		}
+	})
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current users ' + users.join(', '),
+		timestamp: moment().valueOf()
+	})
+}
+
+io.on('connection', function(socket) {
 	console.log("user connected to socket io");
 
-	socket.on('disconnect', function(){
+	socket.on('disconnect', function() {
 		var userData = clientInfo[socket.id];
-		if (typeof userData !=='undefined'){
+		if (typeof userData !== 'undefined') {
 			socket.leave(userData.room);
-			io.to(userData.room).emit('message',{
-				name : 'System',
-				text : userData.name + " has left",
-				timestamp : moment().valueOf()	
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + " has left",
+				timestamp: moment().valueOf()
 			});
 			delete clientInfo[socket.id];
 		}
 
 	})
 
-	socket.on('joinRoom' , function (req){
-		clientInfo[socket.id]= req;
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
 		socket.join(req.room);
-		socket.broadcast.to(req.room).emit('message' , {
-			name : 'System Admin' ,
-			text : req.name + ' has joined',
-			timestamp : moment().valueOf()
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System Admin',
+			text: req.name + ' has joined',
+			timestamp: moment().valueOf()
 		})
 	});
 
-	socket.on('message', function (message){
-		console.log(message);
+	socket.on('message', function(message) {
+
 		console.log('Message received ' + message.text);
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message' , message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	})
 
-	socket.emit('message',{
-		name : 'System',
+	socket.emit('message', {
+		name: 'System',
 		text: 'welcome to the world of chat application',
-		timestamp : moment().valueOf()
+		timestamp: moment().valueOf()
 	});
 
 });
 
-app.use(express.static(__dirname +"/public"));
+app.use(express.static(__dirname + "/public"));
 
-http.listen(PORT , function (){
+http.listen(PORT, function() {
 	console.log("server has been started")
 })
